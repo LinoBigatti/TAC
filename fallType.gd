4293969597cfac483c45b88101c9e7c370905275
Lino Bigatti <linobigatti@protonmail.com>
2021-05-09 21:02:38 -0300

    Add a biavo chart generator.

---
+++ fallType.gd
@@ -0,0 +1,35 @@
+# Any script that interacts with the Project Heartbeat API is licensed under the
+# AGPLv3 for the general public and also gives an exclusive, royalty-free license
+# for EIRTeam to incorporate it in the game
+
+
+# 
+# The power of the dark side in one script
+# By Lino
+#
+
+
+extends ScriptRunnerScript # Do not remove this
+
+func run_script() -> int:
+	# Get selected timing points
+	var selected_timing_points := get_selected_timing_points()
+	
+	# First row
+	var firstRow = 672 
+	
+	var yPos = 918
+
+	# Iterate over them
+	for point in selected_timing_points:
+		var newPos = point.position
+		newPos.x = firstRow
+		newPos.x += 192 * point.note_type
+		newPos.y = yPos
+		set_timing_point_property(point, "position", newPos)
+		set_timing_point_property(point, "entry_angle", -90)
+		set_timing_point_property(point, "oscillation_amplitude", 0)
+	
+	# Return OK to apply the script's changes, return anything else (such as -1)
+	# to cancel it
+	return OK
